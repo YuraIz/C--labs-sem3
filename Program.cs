dabbce176245c6f3890ba861be9fa41dae8da6a5
YuraIz <7516890@gmail.com>
2021-12-27 22:35:59 +0300

    Merge conflicts finished

--- Program.cs
+++ Program.cs
@@ -1,44 +1,4 @@
-<<<<<<< HEAD
-﻿using System;
-using _053505_Izmer_lab6.Collections;
-using _053505_Izmer_lab6.Entities;
-
-namespace _053505_Izmer_lab6
-{
-    class Program
-    {
-        static int Main()
-        {
-            Journal journal = new();
-
-            //Creating product base and adding products
-            ProductBase pb = new(new MyCustomCollection<Product>(new Product("Regular thing", 0.99)));
-            journal.SaveInfo("New product: ", "Regular thing");
-            pb.ListChanged += journal.SaveInfo;
-            pb.NewOrder += (customer, product) => Console.WriteLine($"Customer {customer} ordered {product}");
-
-            pb.AddProduct(new Product("Another thing", 0.49));
-
-            //Making orders
-            pb.MakeNewOrder(new Customer("Regular human"), pb.FindProduct("Regular thing")!);
-            pb.MakeNewOrder(new Customer("Alien"), new[] { pb.FindProduct("Another thing")!, pb.FindProduct("Regular thing")! });
-
-            //Getting information
-            Console.WriteLine("Human's order:");
-            pb.PrintOrderOfCustomer("Regular human");
-            Console.WriteLine("Alien's order:");
-            pb.PrintOrderOfCustomer("Alien");
-            pb.PrintSumOfCustomer("Alien");
-
-            //Printing journal
-            Console.WriteLine("Registered events:");
-            journal.PrintEvents();
-            return 0;
-        }
-    }
-}
-=======
-﻿using System.Text.Json;
+using System.Text.Json;
 using Domain.Entities;
 
 Serializer serializer = new();
@@ -76,4 +36,3 @@ var libsFromLinq = serializer.DeSerializeByLINQ("./libraries-linq.xml");
 Console.WriteLine("JSON:" + (orig.Equals(JsonSerializer.Serialize<IEnumerable<Library>>(libsFromJsom)) ? "Success" : "Fail"));
 Console.WriteLine("XML:" + (orig.Equals(JsonSerializer.Serialize<IEnumerable<Library>>(libsFromXml)) ? "Success" : "Fail"));
 Console.WriteLine("LINQ:" + (orig.Equals(JsonSerializer.Serialize<IEnumerable<Library>>(libsFromLinq)) ? "Success" : "Fail"));
->>>>>>> 5d271a3 (lab9 finished)
